c57f63b9590b56102deac2d022c5727f92748fd7
ivan koop <ivankoop3@gmail.com>
2018-08-08 11:03:54 -0400

    initial Commit

---
+++ index.php
@@ -0,0 +1,442 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Ivan Koop / Developer</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link href="vendor/devicons/css/devicons.min.css" rel="stylesheet">
+    <link href="vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/resume.min.css" rel="stylesheet">
+
+  </head>
+
+  <body id="page-top">
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">
+        <span class="d-none d-lg-block">
+          <img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="img/profile.png" alt="">
+        </span>
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#about">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#experience">Experience</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#education">Education</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#skills">Skills</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#interests">Interests</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#awards">Awards</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="container-fluid p-0">
+
+      <section class="resume-section p-3 p-lg-5 d-flex d-column" id="about">
+        <div class="my-auto">
+          <h1 class="mb-0">Ivan
+            <span class="text-primary">Koop</span>
+          </h1>
+          <div class="subheading mb-5">Asunción · Paraguay · (+595) 981-820-920 ·
+            <a href="mailto:name@email.com">ivankoop3@gmail.com</a>
+          </div>
+          <p class="mb-5">Passionate self-taught developer, 23 years old.
+          <br>
+           Experienced with a wide range of frontend & backend technologies,<br> specialized on native ios and android development.</p>
+          
+          <ul class="list-inline list-social-icons mb-0">
+            <li class="list-inline-item">
+              <a href="https://www.facebook.com/vikoopm" target="_blank">
+                <span class="fa-stack fa-lg">
+                  <i class="fa fa-circle fa-stack-2x"></i>
+                  <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
+                </span>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="https://twitter.com/IvanK013" target="_blank">
+                <span class="fa-stack fa-lg">
+                  <i class="fa fa-circle fa-stack-2x"></i>
+                  <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
+                </span>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="https://www.linkedin.com/in/ivankoop/" target="_blank">
+                <span class="fa-stack fa-lg">
+                  <i class="fa fa-circle fa-stack-2x"></i>
+                  <i class="fa fa-linkedin fa-stack-1x fa-inverse"></i>
+                </span>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="https://github.com/ivankoop" target="_blank">
+                <span class="fa-stack fa-lg">
+                  <i class="fa fa-circle fa-stack-2x"></i>
+                  <i class="fa fa-github fa-stack-1x fa-inverse"></i>
+                </span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
+
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="experience">
+        <div class="my-auto">
+          <h2 class="mb-5">Experience</h2>
+
+          <div class="resume-item d-flex flex-column flex-md-row mb-5">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">Full Stack & Mobile Developer Contractor</h3>
+              <div class="subheading mb-3">Independent Contractor</div>
+              <ul>
+                <li>
+                  Co-founder & Creator of a startup app for travel agencies. (working on part time)
+                </li>
+              </ul>
+            </div>
+            <div class="resume-date text-md-right">
+              <span class="text-primary">July 2018 - Present</span>
+            </div>
+          </div>
+
+          <div class="resume-item d-flex flex-column flex-md-row mb-5">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">Full Stack & Mobile Developer</h3>
+              <div class="subheading mb-3">Teixidó & Co</div>
+              <ul>    
+                <li>
+                  Led the development and adoption of React for a big News website.
+                </li>
+                <li>
+                  Developed & maintained android (java) - ios (Swift).
+                </li>
+                <li>
+                  Developed & maintained back-end - front-end.
+                </li>
+              </ul>
+            </div>
+            <div class="resume-date text-md-right">
+              <span class="text-primary">June 2016 - July 2018</span>
+            </div>
+          </div>
+
+          <div class="resume-item d-flex flex-column flex-md-row mb-5">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">Mobile Developer</h3>
+              <div class="subheading mb-3">Digital Media Company / POPS</div>
+              <ul>
+                <li>
+                  Led the development of an android app using java.
+                </li>
+              </ul>
+              <!--<p>
+                At this company I was hired for one specific project, it was a native android application called WakeApp.
+                The app was a success in my country and was published in many local newspapers:
+              </p>
+              <ul>
+                <li>
+                  <a href="http://www.hoy.com.py/tecnologia/paraguayos-lanzan-app-para-dormir-en-los-buses-y-no-pasar-la-parada" target="_blank">HOY News</a>
+                </li>
+                <li>
+                  <a href="http://www.abc.com.py/edicion-impresa/economia/una-aplicacion-para-usuarios-del-transporte-1525258.html" target="_blank">ABC News</a>
+                </li>
+                <li>
+                  <a href="http://www.ultimahora.com/crean-app-dormir-el-bus-pasarse-la-parada-n1029117.html" target="_blank">Ultima Hora News</a>
+                </li>
+                <li>
+                  <a href="http://www.nanduti.com.py/2016/10/05/dormi-tranquilo-bus-despertate-wake-app/" target="_blank">Radio Station Ñanduti</a>
+                </li>
+                <li>
+                  <a href="http://www.cronica.com.py/2016/10/06/ya-no-vas-pasar-parada/" target="_blank">Cronica News</a>
+                </li>
+              </ul>-->
+            </div>
+            <div class="resume-date text-md-right">
+              <span class="text-primary">March 2016 - May 2016 </span>
+            </div>
+          </div>
+
+          <div class="resume-item d-flex flex-column flex-md-row mb-5">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">Freelance Full Stack Developer</h3>
+                <div class="subheading mb-3">Self employed</div>
+                <ul>
+                  <li>
+                    Full Stack development for a mechanical company, using ajax, php & mysql.
+                  </li>
+                  <li>
+                    Small 2d html5 video games using phaserjs.
+                  </li>
+                </ul>
+              <!--<p>This was like my mid crisis developer experience, I was like "Holy %&!$, software development is not a joke"
+                It started to be a passion for me and I started to read blogs and learn a lot in my free time.
+                Learned how to use git,Sql databases, backend services with php, etc. At this moment I started my first personal project
+                that was for my parents' company, it was a management software for a Car mechanical company, I've done that project using
+                php with mysql as backend and jquery with ajax requests for the frontend. I've made some timelapse videos of the development:
+              </p>
+              <ul>
+                <li>
+                  <a href="https://www.youtube.com/watch?v=Y_9SwDFd_9Y" target="_blank">Programming Motivation VIDEO</a>
+                </li>
+                <li>
+                  <a href="https://www.youtube.com/watch?v=kiT33CceQzY" target="_blank">Speed Coding Jquery with PHP</a>
+                </li>
+              </ul>-->
+            </div>
+            <div class="resume-date text-md-right">
+              <span class="text-primary">Oct 2015 - Mar 2016</span>
+            </div>
+          </div>
+
+          <div class="resume-item d-flex flex-column flex-md-row">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">Junior Frontend & Mobile Developer</h3>
+              <div class="subheading mb-3">Digital Media Company / Noollab</div>
+              <ul>
+                <li>Web development of small landing sites using php & mysql.</li>
+                <li>Mobile development of an hybrid app using sencha touch.</li>
+              </ul>
+              <!--<p>
+                This was my first job as a developer, Here I worked with small websites doing basic html/css and jquery.
+                I had a tutor that taught me all the basics of web development.
+                Also, I started working with Mobile Development with a tool called Sencha Touch that worked with cordova for exporting to iOS & Android.
+              </p>-->
+            </div>
+            <div class="resume-date text-md-right">
+              <span class="text-primary">Feb 2015 - Oct 2015</span>
+            </div>
+          </div>
+
+        </div>
+
+      </section>
+
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="education">
+        <div class="my-auto">
+          <h2 class="mb-5">Education</h2>
+
+          <div class="resume-item d-flex flex-column flex-md-row mb-5">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">Self Taught</h3>
+              <div class="subheading mb-3">Software Development</div>
+              <div>Frontend - Backend - Native Mobile Development /  IOS - Android</div>
+            </div>
+            <div class="resume-date text-md-right">
+              <span class="text-primary">Feb 2014 - present</span>
+            </div>
+          </div>
+
+          <div class="resume-item d-flex flex-column flex-md-row mb-5">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">Catholic University of Asunción Paraguay</h3>
+              <div class="subheading mb-3">Computer Science</div>
+              <div>Algorithms & Data Structures</div>
+              <p>GPA: 3.83</p>
+            </div>
+            <div class="resume-date text-md-right">
+              <span class="text-primary">Feb 2014 - April 2016</span>
+            </div>
+          </div>
+
+          <div class="resume-item d-flex flex-column flex-md-row">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">International High School of Asunción Paraguay</h3>
+              <div class="subheading mb-3">Degree in business administration</div>
+              <p>GPA: 4.21</p>
+            </div>
+            <div class="resume-date text-md-right">
+              <span class="text-primary">March 1999 - Dec 2013</span>
+            </div>
+          </div>
+
+        </div>
+      </section>
+
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="skills">
+        <div class="my-auto">
+          <h2 class="mb-5">Skills</h2>
+
+          <div class="subheading mb-3">Programming Languages &amp; Tools</div>
+          <ul class="list-inline list-icons">
+              <li class="list-inline-item">
+                <i class="devicons devicons-swift"></i>
+              </li>
+              <li class="list-inline-item">
+                <i class="devicons devicons-java"></i>
+              </li>
+              <li class="list-inline-item">
+                <i class="devicons devicons-php"></i>
+              </li>
+              <li class="list-inline-item">
+                <i class="devicons devicons-javascript"></i>
+              </li>
+              <li class="list-inline-item">
+                <i class="devicons devicons-git"></i>
+              </li>
+              <li class="list-inline-item">
+                <i class="devicons devicons-codeigniter"></i>
+              </li>
+              <li class="list-inline-item">
+                <i class="devicons devicons-nodejs"></i>
+              </li>
+              <li class="list-inline-item">
+                <i class="devicons devicons-nginx"></i>
+              </li>
+              <li class="list-inline-item">
+                <i class="devicons devicons-redis"></i>
+              </li>
+              <li class="list-inline-item">
+                <i class="devicons devicons-linux"></i>
+              </li>
+            <li class="list-inline-item">
+              <i class="devicons devicons-html5"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicons devicons-css3"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicons devicons-react"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicons devicons-jquery"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicons devicons-sass"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicons devicons-bootstrap"></i>
+            </li>
+
+            <li class="list-inline-item">
+              <i class="devicons devicons-gulp"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicons devicons-npm"></i>
+            </li>
+          </ul>
+
+          <div class="subheading mb-3">Languages</div>
+          <ul class="fa-ul mb-0">
+            <li>
+              <i class="fa-li fa fa-check"></i>
+              Spanish / speak - read - write
+            </li>
+            <li>
+              <i class="fa-li fa fa-check"></i>
+              English / speak (not fluent) - read - write
+            </li>
+          </ul>
+        </div>
+      </section>
+
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="interests">
+        <div class="my-auto">
+          <h2 class="mb-5">Interests</h2>
+          <p>
+            Apart from being a developer I love to be with my Family and my dogs,
+            <br>
+            play videogames and go out with my friends,
+            <br>
+            some weekends I prefer to code small scripts just for fun and
+            <br>
+            pentesting websites & apps just for curiosity. Reported some vulnerabilities.
+          </p>
+        </div>
+      </section>
+
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="awards">
+        <div class="my-auto">
+          <h2 class="mb-5">Awards &amp; Certifications</h2>
+          <ul class="fa-ul mb-0">
+            <li>
+              <a href="https://courses.edx.org/certificates/942095c1a18b413fa30cfa273a5d0657" target="_blank">
+                <i class="fa-li fa fa-certificate text-warning"></i>
+                Data Science With R - HarvardX Certification
+              </a>
+            </li>
+            <li>
+              <a href="https://courses.edx.org/certificates/ffbc662f31aa4053a1329069a297f437" target="_blank">
+                <i class="fa-li fa fa-certificate text-warning"></i>
+                Introduction to ReactJs - Microsoft Certification
+              </a>
+            </li>
+            <li>
+              <a href="https://www.testdome.com/cert/aek2kr6z2e" target="_blank">
+                <i class="fa-li fa fa-certificate text-warning"></i>
+                HTML/CSS, JS and SQL - Certification
+              </a>
+            </li>
+            <li>
+              <a href="https://www.testdome.com/cert/56ttdj7sjj" target="_blank">
+                <i class="fa-li fa fa-certificate text-warning"></i>
+                Javascript top 10% Score - Certification
+              </a>
+            </li>
+            <li>
+              <a href="https://www.testdome.com/cert/zswuzeb1xc" target="_blank">
+                <i class="fa-li fa fa-certificate text-warning"></i>
+                PHP - Certification
+              </a>
+            </li>
+            <li>
+              <a href="https://www.testdome.com/cert/unskv5sxcw" target="_blank">
+                <i class="fa-li fa fa-certificate text-warning"></i>
+                Java - Certification
+              </a>
+            </li>
+            <li>
+              <a href="uploads/GGJCertificado_ivan_koop.png" target="_blank">
+                <i class="fa-li fa fa-certificate text-warning"></i>
+                Global Game Jam 2018 - Certification
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
+
+    </div>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="js/resume.min.js"></script>
+
+  </body>
+
+</html>
