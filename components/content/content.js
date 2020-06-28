70ec428a66c4358daaf03bba2e77f9354b94adc4
ivan koop <ivankoop3@gmail.com>
2020-06-28 00:59:00 -0400

    Added experiences section.

--- components/content/content.js
+++ components/content/content.js
@@ -1,9 +1,15 @@
 import styles from './content.module.css';
+import Experience from './experience/experience';
 
 export default function Content(props) {
+  const { experiences } = props;
   return (
     <div className={styles.contentRoot}>
-      
+      {experiences.map((experience, index) => (
+        <div key={`experience-${index}`}>
+          <Experience data={experience} />
+        </div>
+      ))}
     </div>
   );
 }
--- pages/api/experiences.js
+++ pages/api/experiences.js
@@ -3,6 +3,7 @@ import firebase from '../../lib/firebase';
 export default function experiences(req, res) {
   firebase
     .collection('work-experience')
+    .orderBy("order", "desc")
     .get()
     .then((snapshot) => {
       const experiences = [];
