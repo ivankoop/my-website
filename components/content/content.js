fe37309831a015962ed0dacea2f51197c993c6ad
Ivan Koop <ivankoop3@gmail.com>
2020-08-21 10:54:43 -0400

    IKW-1: responsive header added.

--- components/content/content.js
+++ components/content/content.js
@@ -1,10 +1,16 @@
 import styles from './content.module.css';
 import Experience from './experience/experience';
+import HeaderTitles from '../shared/HeaderTitles';
+import Divider from '../shared/divider';
 
 export default function Content(props) {
   const { experiences } = props;
   return (
     <div className={styles.contentRoot}>
+      <div className={styles.responsiveHeader}>
+        <HeaderTitles />
+        <Divider/>
+      </div>
       {experiences.map((experience, index) => (
         <div key={`experience-${index}`}>
           <Experience data={experience} />
--- components/sidebar/sidebar.js
+++ components/sidebar/sidebar.js
@@ -1,36 +1,30 @@
-import styles from "./sidebar.module.css";
-import Contributions from "./contributions/contributions"
-import Certifications from "./certifications/certifications"
-import Divider from "../shared/divider"
+import styles from './sidebar.module.css';
+import Contributions from './contributions/contributions';
+import Certifications from './certifications/certifications';
+import Divider from '../shared/divider';
+import HeaderTitles from '../shared/HeaderTitles';
 
-const linkedinUrl = "https://www.linkedin.com/in/ivankoop/"
-const githubUrl = "https://github.com/ivankoop"
+
 
 export default function SideBar() {
   return (
     <div className={styles.sidebarRoot}>
-      <h1 className={styles.title}>Ivan Koop</h1>
-      <h2 className={styles.subtitle}>Software Developer</h2>
-      <div className={styles.iconsRow}>
-        <a href={linkedinUrl} target="_blank" className={styles.iconCont}>
-          <i className="icon">&#xf30c;</i>
-        </a>
-        <a href={githubUrl} target="_blank" className={styles.iconCont}>
-          <i className="icon">&#xf113;</i>
-        </a>
-      </div>
-      <Divider/>
+      <HeaderTitles />
+      <Divider />
       <p className={styles.description}>
         Passionate self-taught developer, 25 years old. Experienced with a wide
         range of frontend & backend technologies, specialized on Frontend
         development.
       </p>
-      <Divider/>
-      <Contributions/>
-      <Divider/>
-      <Certifications/>
+      <Divider />
+      <Contributions />
+      <Divider />
+      <Certifications />
       <div className={styles.sideBarDivider}></div>
-      <div className={styles.copyRight}>Created with ❤️ by <strong>Ivan Koop</strong>, Powered by <strong>NextJs</strong></div>
+      <div className={styles.copyRight}>
+        Created with ❤️ by <strong>Ivan Koop</strong>, Powered by{' '}
+        <strong>NextJs</strong>
+      </div>
     </div>
   );
 }
