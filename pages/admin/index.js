3b63f023689bb04a6bcd47dd6d0dd447b66ea8a2
Ivan Koop <ivan@clearsumm.it>
2022-01-08 18:03:10 -0300

    Fauna db auth

---
+++ pages/admin/index.js
@@ -0,0 +1,23 @@
+import Layout from '../../components/layout/layout';
+import { getAuthCookie } from '../../lib/auth-cookies';
+import useSWR from 'swr';
+import { useRouter } from 'next/router';
+
+function Admin({ token }) {
+  const router = useRouter();
+  const { data: user, error } = useSWR('/api/user');
+
+  if (error) {
+    router.push('/admin/login');
+  }
+
+  console.log('user', user);
+
+  return <Layout>Admin</Layout>;
+}
+
+export async function getServerSideProps(ctx) {
+  const token = getAuthCookie(ctx.req);
+  return { props: { token: token || null } };
+}
+export default Admin;
